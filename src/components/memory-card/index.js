f065315becaaae080bf54ba5e95aaae2e8908b39
Mario Oliveira <mario.oliveirag12@gmail>
2019-01-26 21:16:27 +0000

    [025] - Desafio 1

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,27 +1,14 @@
-const createMemoryCard =  ()  => `
-    <article class="memory-card"">
-        <img 
-            src="img/icon-collabcode.svg" 
-            alt="Gueio - Mascote da CollabCode"
-            class="icon"
-             onClick="hardleClick()"
-            >
-    </article> `;
+const createMemoryCard = (src,nameClass,alt) => {
+  return `
+    <article class="memory-card ${nameClass}">
+            <img 
+                src="${src}" 
+                alt="${alt}"
+                class="icon"
+                onClick="hardleClick()"
+                >
+        </article> 
+        `;
+};
 
-
-
-const createMemoryCardFront = () =>   `
-    <article class="memory-card -front">
-         <img 
-            class="icon" 
-            src="img/icon-js.png" 
-            alt="Js"
-            onClick="hardleClick()"
-            >
-    </article> `;
-    
-
-
-function hardleClick(){
-  console.log('AE');
-}
+const hardleClick = () => console.log("AE");
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,28 +1,63 @@
-
-const $root = document.querySelector('#root');
+const $root = document.querySelector("#root");
 const $cardsWrapper = createCardsWrapper();
-const $memoryCard = createMemoryCard();
-const $createMemoryCardFront = createMemoryCardFront();
 
+const $memoryCard = createMemoryCard(
+  "img/icon-collabcode.svg",
+  "Gueio - Mascote da CollabCode"
+);
+const $createMemoryCardFront = createMemoryCard(
+  "img/icon-js.png",
+  "-front",
+  "Front End Javascript"
+);
 
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
+const $createMemoryCardC = createMemoryCard("img/icon-c.png", "-front", "C#");
+const $createMemoryCardJava = createMemoryCard(
+  "img/icon-java.png",
+  "-front",
+  "Java"
+);
+const $createMemoryCardCabeca = createMemoryCard(
+  "img/icon-cabeca.png",
+  "-front",
+  "Code"
+);
+const $createMemoryCardPhp = createMemoryCard(
+  "img/icon-php.png",
+  "-front",
+  "PHP"
+);
+const $createMemoryCardCelular = createMemoryCard(
+  "img/icon-celular.png",
+  "-front",
+  "Celular"
+);
+const $createMemoryCardBug = createMemoryCard(
+  "img/icon-bug.png",
+  "-front",
+  "Bugs"
+);
+const $createMemoryCardResposive = createMemoryCard(
+  "img/icon-responsivo.png",
+  "-front",
+  "Responsivo"
+);
+
+const $createMemoryCardWoman = createMemoryCard("img/icon-woman.png","-front", "Woman Devs");
+
+const $createMemoryCardimgSettings = createMemoryCard("img/icon-settings.png", "-front", "Settings");
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $createMemoryCardPhp);
+$cardsWrapper.insertAdjacentHTML("beforeend", $createMemoryCardCabeca);
+$cardsWrapper.insertAdjacentHTML("beforeend", $createMemoryCardJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $createMemoryCardC);
+$cardsWrapper.insertAdjacentHTML("beforeend", $createMemoryCardCelular);
 $cardsWrapper.insertAdjacentHTML("beforeend", $createMemoryCardFront);
-$cardsWrapper.insertAdjacentHTML("beforeend", $createMemoryCardFront);
-
-$root.insertAdjacentElement('beforeend', $cardsWrapper);
-
-
-
+$cardsWrapper.insertAdjacentHTML("beforeend", $createMemoryCardBug);
+$cardsWrapper.insertAdjacentHTML("beforeend", $createMemoryCardResposive);
+$cardsWrapper.insertAdjacentHTML("beforeend", $createMemoryCardWoman);
+$cardsWrapper.insertAdjacentHTML("beforeend", $createMemoryCardimgSettings);
 
 
-
-
-
-
-
-
+$root.insertAdjacentElement("beforeend", $cardsWrapper);
