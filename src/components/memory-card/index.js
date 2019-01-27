4a43a246ed56356e6d7aa5f82ef9c75f321a4f4c
Mario Oliveira <mario.oliveirag12@gmail>
2019-01-27 23:16:17 +0000

    [026] - Desafio único

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,3 +1,62 @@
+
+const $heade = document.querySelector('head');
+const $style= document.createElement('style');
+ 
+$style.textContent = `
+.memory-card {
+  width: 155px;
+  height: 155px;
+  background-color: #f25a70;
+  border-radius: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  margin-bottom: 15px;
+  cursor: pointer;
+}
+
+.memory-card > .icon {
+  width: 100px;
+  height: 100px;
+}
+
+.memory-card.-front {
+  width: 155px;
+  height: 155px;
+  background: #fff;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  border-radius: 30px;
+  display: flex;
+  justify-content: center;
+  position: relative;
+}
+
+.memory-card.-front::before {
+  width: 97px;
+  height: 94px;
+  background-color: #d4d4d4;
+  border-radius: 50%;
+  content: "";
+  position: absolute;
+}
+
+.memory-card.-front .icon {
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  transform: translateY(-10px);
+}
+
+
+
+`;
+
+$heade.insertBefore($style,null);
+
+
+
+
 const createMemoryCard =  ({src, alt, nameClass})  => `
     <article class="memory-card ${nameClass}">
             <img 
@@ -8,5 +67,7 @@ const createMemoryCard =  ({src, alt, nameClass})  => `
                 >
         </article> 
       `;
+
+
 
 const hardleClick = () => console.log("AE");
